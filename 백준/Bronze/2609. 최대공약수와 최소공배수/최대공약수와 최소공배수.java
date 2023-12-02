d71a4b45310009be3d3cb73e6e870aa4b3592d02
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2023-12-02 23:51:04 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 108 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int mul = a * b;
+		
+		while (b != 0) {
+			a = a % b;
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		
+		System.out.println(a);
+		System.out.println((int) mul / a);
+	}
+	
+}
