2d4126bb04e4b04e96f6a7650fb3373d756123cc
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2023-12-28 19:54:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 620 ms, Memory: 50324 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,9 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 // 프림 : 모든 정점들을 연결했을 때, 연결되는 가중치의 최소 합
@@ -13,18 +13,20 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node {
-		int vertex, weight;
-
-		public Node(int vertex, int weight) {
-			this.vertex = vertex;
-			this.weight = weight;
-		}
+	static class Edge{
+		int from;
+		int to;
+		int w;
+		public Edge(int from, int to, int w) {
+			this.from = from;
+			this.to = to;
+			this.w = w;
+		}	
 	}
 
-	static List<List<Node>> graph;
-	static boolean[] visit;
+	static List<Edge> edgeList;
 	static int sum;
+	static int[] parent;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,12 +36,13 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken()); // 정점의 수
 		int M = Integer.parseInt(st.nextToken()); // 간선의 수
 
-		graph = new ArrayList<>();
-		visit = new boolean[N + 1];
+		edgeList = new ArrayList<>();
 		sum = 0;
-
-		for (int i = 0; i < N + 1; i++) {
-			graph.add(new ArrayList<>());
+		
+		parent = new int[N+1];
+		
+		for(int i =1; i<=N; i++) {
+			parent[i] = i;
 		}
 
 		for (int i = 0; i < M; i++) {
@@ -48,40 +51,52 @@ public class Main {
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 
-			graph.get(u).add(new Node(v, w));
-			graph.get(v).add(new Node(u, w));
+			edgeList.add(new Edge(u,v,w));
 		}
-		//// 연결까지 끝
+		
+		Collections.sort(edgeList, new Comparator<Edge>() {
 
-		bfs(new Node(1, 0));
+			@Override
+			public int compare(Edge o1,Edge o2) {
+				return o1.w - o2.w;
+			}
+			
+		});
+		//// 연결까지 끝
+		int cnt = 0;
+		
+		for(Edge edge : edgeList) {
+			if(union(edge.from, edge.to)) {
+				sum += edge.w;
+				if(++cnt == M-1) {
+					break;
+				}
+			}
+		}
 
 		System.out.println(sum);
 
 	}
-
-	public static void bfs(Node start) {
-		PriorityQueue<Node> queue = new PriorityQueue<>(new Comparator<Node>() {
-
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o1.weight - o2.weight;
-			}
-		});
-
-		queue.offer(start);
-
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-
-			if (!visit[now.vertex]) {
-				visit[now.vertex] = true;
-				sum += now.weight;
-
-				for (int i = 0; i < graph.get(now.vertex).size(); i++) {
-					Node next = graph.get(now.vertex).get(i);
-					queue.add(next);
-				}
-			}
+	public static int find(int x) {
+		if(parent[x] == x) {
+			return x;
 		}
+		return parent[x] = find(parent[x]);
+	}
+	
+	public static boolean union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x == y) {
+			return false;
+		}
+		
+		if(x <= y) {
+			parent[y] = x;
+		} else {
+			parent[x] = y;
+		}
+		return true;			
 	}
 }
