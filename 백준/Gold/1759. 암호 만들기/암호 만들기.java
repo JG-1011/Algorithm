df941b725363644f187fee5ed7c3f650c4d953b5
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-01-02 23:13:22 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int L, C;
+	static char[] arr, ans;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		ans = new char[L];
+		arr = new char[C];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < C; i++) {
+			arr[i] = st.nextToken().charAt(0);
+		}
+
+		Arrays.sort(arr);
+
+		combi(0, 0);
+
+		System.out.println(sb);
+	}
+
+	public static void combi(int start, int depth) {
+		if (depth == L) {
+			if (check()) {
+				for (char a : ans) {
+					sb.append(a);
+				}
+				sb.append("\n");
+				return;
+			}
+			return;
+		}
+
+		for (int i = start; i < C; i++) {
+			ans[depth] = arr[i];
+			combi(i + 1, depth + 1);
+		}
+	}
+
+	public static boolean check() {
+		int aeiou = 0;
+		int other = 0;
+
+		for (int i = 0; i < L; i++) {
+			if (ans[i] == 'a' || ans[i] == 'e' || ans[i] == 'i' || ans[i] == 'o' || ans[i] == 'u') {
+				aeiou++;
+			} else {
+				other++;
+			}
+		}
+
+		if (aeiou >= 1 && other >= 2) {
+			return true;
+		}
+		return false;
+	}
+}
