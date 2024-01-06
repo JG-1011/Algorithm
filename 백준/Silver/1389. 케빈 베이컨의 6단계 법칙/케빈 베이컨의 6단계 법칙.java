7c95f56e81b05048c7f5d45fc207db82a98ae204
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-01-06 22:07:26 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 88 ms, Memory: 11776 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // input
+    private static BufferedReader br;
+
+    // variables
+    private static final int INF = Integer.MAX_VALUE / 4;
+    private static int N, M;
+    private static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input();
+
+        bw.write(solve());
+        bw.close();
+    } // End of main()
+
+    private static String solve() {
+        StringBuilder sb = new StringBuilder();
+
+        floyd();
+
+        int ans = -1;
+        int min = INF;
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += arr[i][j];
+            }
+
+            if (min > sum) {
+                min = sum;
+                ans = i;
+            }
+        }
+
+        sb.append(ans);
+        return sb.toString();
+    } // End of solve()
+
+    private static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    // 최단경로 초기화
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+    } // End of floyd()
+
+    private static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                arr[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+    } // End of input()
+} // End of Main class
