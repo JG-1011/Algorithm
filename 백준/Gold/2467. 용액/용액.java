60e3f905068131aa8df73f8cc311b2ce2202d13d
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-09-21 14:42:12 +0900

    [Gold V] Title: 용액, Time: 236 ms, Memory: 31524 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -3,23 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 해결방법
- * hashMap을 사용해야 할 거 같은데?
- * 두값을 저장하고 결과값을 넣는다?
- * Hash<결과값, 결과값을 만든 두 값>
- * 정렬하고 > 이게 문제네
- *
- * 두개를 더한 배열을 만들고 > 정렬 > 0에 가까운 수를 찾았다고 가정
- * 그럼 그 결과값을 찾기 무언가를 한다? 너무 비효율적 아닌가?
- *
- *
- *
- * */
 public class Main {
 
     static int[] arr;
-    static int N, max;
+    static int N, minSum;
     static int[] ans;
 
     public static void main(String[] args) throws IOException {
@@ -27,7 +14,6 @@ public class Main {
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
@@ -36,38 +22,37 @@ public class Main {
         }
 
         ans = new int[2];
-        max = Integer.MAX_VALUE;
-        binarySearch();
+        minSum = Integer.MAX_VALUE;
 
+        // 두 포인터 사용
+        twoPointer();
+
+        // 결과 출력 (작은 값부터 출력)
         System.out.println(ans[0] + " " + ans[1]);
-
     }
 
-    private static void binarySearch() {
-        for (int i = 0; i < N; i++) {
-            int left = i + 1;
-            int right = N - 1;
+    private static void twoPointer() {
+        int left = 0;
+        int right = N - 1;
 
-            while (left <= right) {
-                int mid = (left + right) / 2;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
 
-                int sum = arr[i] + arr[mid];
-
-                if (Math.abs(sum) < max) {
-                    ans[0] = arr[i];
-                    ans[1] = arr[mid];
-                    max = Math.abs(sum);
-                }
-
-                if (sum < 0) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-
+            // 0에 가까운 값을 찾으면 갱신
+            if (Math.abs(sum) < minSum) {
+                minSum = Math.abs(sum);
+                ans[0] = arr[left];
+                ans[1] = arr[right];
             }
 
+            // 용액의 합이 음수면, 더 큰 값을 찾아야 하므로 left를 증가
+            if (sum < 0) {
+                left++;
+            }
+            // 용액의 합이 양수면, 더 작은 값을 찾아야 하므로 right를 감소
+            else {
+                right--;
+            }
         }
     }
-
 }
