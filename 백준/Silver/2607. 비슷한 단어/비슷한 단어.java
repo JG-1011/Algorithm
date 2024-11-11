58960af6580c5353b175a079b122a8947cd6354f
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-11-12 00:59:22 +0900

    [Silver II] Title: 비슷한 단어, Time: 64 ms, Memory: 11352 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,31 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+/*
+ * 단어와 단어를 비교해야 한다.
+ * charAt을 통해 하나하나 알아보는 방법도 있으나 너무 오래 걸린듯??
+ * int나 boolean 배열을 이용해서 풀어보자
+ *
+ * 인사이트
+ * 같은지 비교할 때 int 배열로 서로 비교해본다
+ * */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine()); // 단어의 개수
         String baseWord = br.readLine(); // 기준이 되는 첫 번째 단어
         int[] baseCount = getAlphabetCount(baseWord); // 첫 번째 단어의 알파벳 개수 배열
-        
+
         int similarCount = 0;
-        
+
         for (int i = 1; i < n; i++) {
             String word = br.readLine();
             int[] wordCount = getAlphabetCount(word);
-            
+
             // 알파벳 개수 차이를 계산
             int difference = calculateDifference(baseCount, wordCount);
-            
+
             // 비슷한 단어인지 판별
             if (isSimilar(difference, baseWord.length(), word.length())) {
                 similarCount++;
             }
         }
-        
+
         System.out.println(similarCount);
     }
 
@@ -33,7 +40,7 @@ public class Main {
     private static int[] getAlphabetCount(String word) {
         int[] count = new int[26]; // 알파벳 개수를 저장할 배열 (A-Z는 26개의 문자)
         for (char c : word.toCharArray()) {
-            count[c - 'A']++;
+            count[c - 'A']++; // 이렇게 하면 A가 인덱스 0이 된다.
         }
         return count;
     }
