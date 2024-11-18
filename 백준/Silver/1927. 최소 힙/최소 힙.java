501e806472bbf3fe3b330c9a868802c45d7348e2
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-11-18 16:26:11 +0900

    [Silver II] Title: 최소 힙, Time: 264 ms, Memory: 25320 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,23 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Integer> queue = new PriorityQueue<>();
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<N; i++) {
-			int val = Integer.parseInt(br.readLine());
-			
-			if(val == 0) {
-				if(queue.isEmpty())
-					System.out.println("0");
-				else
-					System.out.println(queue.poll());
-			}
-			else
-				queue.add(val);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num != 0) {
+                minHeap.add(num);
+            } else {
+                if (minHeap.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(minHeap.poll()).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
