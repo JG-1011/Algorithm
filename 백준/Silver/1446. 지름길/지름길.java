1b1b2f6304787b73fab1d9765988390a91510d56
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2024-11-19 18:17:21 +0900

    [Silver I] Title: 지름길, Time: 80 ms, Memory: 11712 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+ * 1. dp로 풀어보자
+ * */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        List<Road> roads = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+
+            // 유효한 지름길만 추가 (깨알 포인트)
+            if (e <= D) {
+                roads.add(new Road(s, e, l));
+            }
+        }
+
+        int[] dist = new int[D + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
+        for (int i = 0; i <= D; i++) {
+            if (i > 0) {
+                dist[i] = Math.min(dist[i], dist[i - 1] + 1);
+            }
+
+            for (Road road : roads) {
+                if (road.start == i) {
+                    dist[road.end] = Math.min(dist[road.end], dist[i] + road.length);
+                }
+            }
+        }
+
+        System.out.println(dist[D]);
+    }
+}
+
+class Road {
+    int start, end, length;
+
+    public Road(int start, int end, int length) {
+        this.start = start;
+        this.end = end;
+        this.length = length;
+    }
+}
