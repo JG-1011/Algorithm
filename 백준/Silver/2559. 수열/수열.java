0663c7927ed781ed0cf52f7367a8673c8fae0313
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-03 12:54:33 +0900

    [Silver III] Title: 수열, Time: 176 ms, Memory: 22440 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -18,7 +18,6 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 초기 구간 합 계산 (첫 번째 K개의 합)
         int sum = 0;
         for (int i = 0; i < K; i++) {
             sum += arr[i];
@@ -26,9 +25,8 @@ public class Main {
 
         int maxSum = sum;
 
-        // 슬라이딩 윈도우 방식으로 최대 합 계산
         for (int i = K; i < N; i++) {
-            sum = sum - arr[i - K] + arr[i]; // 앞의 값을 빼고 새로운 값을 추가
+            sum = sum - arr[i - K] + arr[i];
             maxSum = Math.max(maxSum, sum);
         }
 
