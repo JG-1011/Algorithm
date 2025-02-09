e2a2743856196dc74619612df0e8303f50f01297
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-10 03:59:54 +0900

    [Silver I] Title: 그림, Time: 308 ms, Memory: 43176 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -5,79 +5,89 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 문제
+ * 그림의 개수와 넓이가 가장 넓은 그림을 출력해라
+ * 1로 연결된 것을 그림이라고 한다.
+ *
+ * 풀이
+ * BFS
+ * 1이면 bfs 들어가서 넓이를 가져와
+ * 카운트는 세고 넓이는 맥스와 비교
+ *
+ */
 public class Main {
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { -0, 0, -1, 1 };
-	static int[][] map;
-	static boolean[][] visit;
-	static int N, M, cnt, area, maxArea;
+    static class Point {
+        int r, c;
 
-	static class Point {
-		int r, c;
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
 
-		public Point(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		map = new int[N][M];
-		visit = new boolean[N][M];
-		cnt = 0;
+        map = new int[N][M];
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 1 && !visit[i][j]) {
-					cnt++;
-					bfs(i, j);
-				}
-			}
-		}
+        int pictureCnt = 0;
+        int maxSize = 0;
+        visited = new boolean[N][M];
 
-		System.out.println(cnt);
-		System.out.println(maxArea);
-	}
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    maxSize = Math.max(bfs(i, j), maxSize);
+                    pictureCnt++;
+                }
+            }
+        }
 
-	public static void bfs(int r, int c) {
-		Queue<Point> queue = new LinkedList<>();
+        System.out.println(pictureCnt);
+        System.out.println(maxSize);
+    }
 
-		queue.add(new Point(r, c));
-		visit[r][c] = true;
-		area = 1;
+    private static int bfs(int r, int c) {
+        Queue<Point> queue = new LinkedList<>();
+        visited[r][c] = true;
+        queue.add(new Point(r, c));
 
-		while (!queue.isEmpty()) {
-			Point p = queue.poll();
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
 
-			for (int d = 0; d < 4; d++) {
-				int nr = p.r + dr[d];
-				int nc = p.c + dc[d];
+            cnt++;
 
-				if (nr < 0 || nc < 0 || nr >= N || nc >= M || map[nr][nc] == 0 || visit[nr][nc]) {
-					continue;
-				}
-				queue.add(new Point(nr, nc));
-				visit[nr][nc] = true;
-				area += 1;
+            for (int d = 0; d < 4; d++) {
+                int nr = now.r + dr[d];
+                int nc = now.c + dc[d];
 
-			}
-		}
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc] || map[nr][nc] == 0) continue;
 
-		maxArea = Math.max(maxArea, area);
-	}
+                visited[nr][nc] = true;
+                queue.add(new Point(nr, nc));
+            }
+        }
 
+        return cnt;
+    }
 }
