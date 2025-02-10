772ff07268a025eaca1320c1e52e35277250919e
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-11 01:43:08 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13608 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,63 +1,93 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-
+/*
+ * 문제
+ * 해충으로부터 보호하기 위해 흰지렁이 구매했음
+ * 지렁이가 인접한 배추로 이동가능 -> 사방탐색
+ * 지렁이가 몇마리 필요한지 알아야 함
+ *
+ * 풀이
+ * BFS돌면서 배추들이 얼마나 나눠져 있는지 확인해야 함
+ * 1 : 배추가 있는 곳
+ * 0 : 없는 곳
+ *
+ */
 public class Main {
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static int N, M, K, cnt;
-	static boolean[][] visit;
-	static int[][] map;
+    static class Point {
+        int r, c;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
 
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int N, M, K;
+    static int[][] map;
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-			visit = new boolean[N][M];
-			map = new int[N][M];
+        int T = Integer.parseInt(br.readLine());
 
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int tmp = Integer.parseInt(st.nextToken());
-				int tmp2 = Integer.parseInt(st.nextToken());
-				map[tmp2][tmp] = 1;
-			}
-			cnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (!visit[i][j] && map[i][j] == 1) {
-						visit[i][j] = true;
-						dfs(i, j);
-						cnt++;
-					}
-				}
-			}
-			System.out.println(cnt);
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-		} // tc
+            map = new int[N][M];
+            visited = new boolean[N][M];
 
-	}
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
-	public static void dfs(int r, int c) {
-		for (int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+                map[y][x] = 1;
+            }
 
-			if (nr < 0 || nc < 0 || nr >= N || nc >= M || visit[nr][nc] || map[nr][nc] == 0) {
-				continue;
-			}
-			visit[nr][nc] = true;
-			dfs(nr, nc);
+            int bugCnt = 0;
 
-		}
-	}
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        bugCnt++;
+                        bfs(i, j);
+                    }
+                }
+            }
+
+            System.out.println(bugCnt);
+        }
+    }
+
+    private static void bfs(int startR, int startC) {
+        Queue<Point> queue = new LinkedList<>();
+
+        visited[startR][startC] = true;
+        queue.add(new Point(startR, startC));
+
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nr = now.r + dr[d];
+                int nc = now.c + dc[d];
+
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc] || map[nr][nc] == 0) continue;
+
+                visited[nr][nc] = true;
+                queue.add(new Point(nr, nc));
+            }
+        }
+
+    }
 }
