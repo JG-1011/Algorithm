f2fee289bfbd5c05baebaf44ea54e633512e4264
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-12 04:07:16 +0900

    [Silver IV] Title: 수들의 합 2, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -3,38 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * 문제
+ * 배열 i~j 연속된 수들을 더해서 M이 되는 경우의 수를 구해라
+ *
+ * 해결
+ * 이분탐색이나 투포인트로 해결하면 될 듯
+ * */
 public class Main {
+    static int[] arr;
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int sum=0;
-        int start=0,end=0,count=0;
+        System.out.println(twoPoint());
 
-        while(true){
-            if(sum>=m){
-                sum-=arr[start++];
-            }
-            else if(end==n){
-                break;
-            }else{
-                sum+=arr[end++];
+    }
+
+    private static int twoPoint() {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int cnt = 0;
+
+        while (right < N) {
+            sum += arr[right];
+
+            while (sum > M) {
+                sum -= arr[left];
+                left++;
             }
 
-            if(sum==m){
-                count++;
-            }
+            if (sum == M) cnt++;
+
+            right++;
         }
-        System.out.println(count);
 
+        return cnt;
     }
 }
