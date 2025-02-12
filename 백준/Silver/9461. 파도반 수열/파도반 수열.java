cf9da45fb9482b58d4e8ceabd987ebf2881f5620
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-13 02:31:06 +0900

    [Silver III] Title: 파도반 수열, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        long[] memo = new long[101];
+    static long[] dp = new long[101]; // 최대 100까지 저장
 
-        memo[1] = memo[2] = memo[3] = 1;
-        memo[4] = memo[5] = 2;
-        for (int i = 6; i <= 100; i++)
-            memo[i] = memo[i - 1] + memo[i - 5];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        for (int tc = 1; tc <= T; tc++)
-            System.out.println(memo[sc.nextInt()]);
+        int T = Integer.parseInt(br.readLine());
 
+        // DP 초기값 설정
+        dp[1] = dp[2] = dp[3] = 1;
+
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
