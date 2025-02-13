7f1e688775ff0e59d4b63bac1ad72db3d8db9dbb
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-13 22:06:03 +0900

    [Gold V] Title: 토마토, Time: 496 ms, Memory: 121332 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,15 +5,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 문제
+ * 토마토 박스 안에 안 익은 토마토가 있음(여러개 존재할 듯)
+ * 안 익은 토마토는 익은 토마토 옆에 있으면 하루 지나 익게 됨(4방탐색)
+ * 며칠이 지나야 다 익는지 최소 일자를 구해라
+ * 모든 토마토가 익은 상태라면 0을 출력, 모두 익지 못하는 상황이면 -1
+ *
+ * 해결
+ * BFS 해결하면 될 듯
+ * 익지 않은 토마토가 있는지 확인해야 할듯 > 익지 않은 토마토 개수를 카운트 해서 나중에 비교
+ *
+ * */
 public class Main {
-    static int N, M, max; // 지도의 가로, 세로 크기와 최대 거리를 저장하는 변수
-    static int[][] map, dp; // 지도 정보와 각 지점까지의 거리 정보를 저장하는 배열
-    static boolean[][] visit; // 방문한 지점을 추적하는 배열
-    static int[] dr = { -1, 1, 0, 0 }; // 상하좌우 이동을 위한 배열
-    static int[] dc = { 0, 0, -1, 1 }; // 상하좌우 이동을 위한 배열
-    static boolean isOk; // 모든 곳이 연결되어 있는지 여부를 확인하는 변수
-    static Queue<Point> queue = new LinkedList<>(); // 좌표를 저장하는 큐
-
     static class Point {
         int r, c;
 
@@ -23,73 +27,68 @@ public class Main {
         }
     }
 
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int N, M, zeroCnt;
+    static int[][] map;
+    static Queue<Point> queue = new LinkedList<>();
+    static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        map = new int[M][N]; // 지도 정보를 저장하는 배열
-        visit = new boolean[M][N]; // 지점 방문 여부를 저장하는 배열
-        dp = new int[M][N]; // 각 지점까지의 거리를 저장하는 배열
-        isOk = false; // 초기에는 연결되지 않았다고 가정
-        max = 0; // 최대 거리 초기화
+        visited = new boolean[N][M];
+        map = new int[N][M];
+        zeroCnt = 0;
 
-        // 지도 정보를 입력받습니다.
-        for (int i = 0; i < M; i++) {
+        // 1:익은, 0:익지않은, -1:빈공간
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-
-                if (map[i][j] == 1) {
-                    queue.add(new Point(i, j)); // 섬의 좌표를 큐에 추가
+                if (map[i][j] == 0) {
+                    zeroCnt++;
+                } else if (map[i][j] == 1) {
+                    visited[i][j] = true;
+                    queue.add(new Point(i, j));
                 }
             }
         }
 
-        // bfs 메서드 호출
-        bfs();
-
-        // 방문하지 않은 0인 지점이 있다면, -1을 출력합니다.
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 0 && !visit[i][j]) {
-                    isOk = true;
-                }
-            }
-        }
-
-        if (isOk) {
-            System.out.println(-1);
-        } else {
-            // 최대 거리를 출력합니다.
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    max = Math.max(max, dp[i][j]);
-                }
-            }
-            System.out.println(max);
-        }
+        System.out.println(zeroCnt == 0 ? 0 : bfs());
     }
 
-    // bfs를 통해 각 지점까지의 거리를 계산하는 메서드
-    public static void bfs() {
+    private static int bfs() {
+        int day = 0;
+
         while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            visit[p.r][p.c] = true;
+            int size = queue.size();
 
-            for (int d = 0; d < 4; d++) {
-                int nr = p.r + dr[d];
-                int nc = p.c + dc[d];
+            while (size-- > 0) {
+                Point now = queue.poll();
 
-                if (nr >= 0 && nc >= 0 && nr < M && nc < N && map[nr][nc] == 0 && !visit[nr][nc]) {
-                    queue.offer(new Point(nr, nc));
-                    dp[nr][nc] = dp[p.r][p.c] + 1; // 현재까지의 거리에 1을 더합니다.
-                    visit[nr][nc] = true;
+                for (int d = 0; d < 4; d++) {
+                    int nr = now.r + dr[d];
+                    int nc = now.c + dc[d];
+
+                    if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+                    if (visited[nr][nc] || map[nr][nc] == -1) continue;
+                    
+                    visited[nr][nc] = true;
+                    queue.add(new Point(nr, nc));
+                    zeroCnt--;
+                    
                 }
             }
+
+            day++;
         }
+
+        return (zeroCnt == 0) ? day - 1 : -1;
     }
 }
