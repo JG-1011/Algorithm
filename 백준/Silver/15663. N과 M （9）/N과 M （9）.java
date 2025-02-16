dbf2281ffd78952b3649bd85ddb3477aba3de305
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-17 06:13:55 +0900

    [Silver II] Title: N과 M (9), Time: 132 ms, Memory: 18632 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,60 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
+/*
+ * 문제
+ * 순열 + 중복 방지
+ * */
 public class Main {
-	static int N, M;
-	static int[] arr;
-	static int[] sortArr;
-	static StringBuilder sb;
-	static boolean[] visit;
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		sortArr = new int[N];
+        arr = new int[N];
+        visited = new boolean[N];
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			sortArr[i] = Integer.parseInt(st.nextToken());
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		Arrays.sort(sortArr);
+        Arrays.sort(arr);
 
-		arr = new int[M]; // 뽑은 수를 담아야 하기 때문에 M
-		visit = new boolean[N];
+        combi(0);
 
-		dfs(0);
-		System.out.println(sb);
-	}
+        System.out.println(sb);
+    }
 
-	public static void dfs(int depth) {
-		if (depth == M) {
-			for (int a : arr) {
-				sb.append(a + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		int before = 0;
-		for (int i = 0; i < N; i++) {
-			if (!visit[i]) {
-				if (before != sortArr[i]) {// 어차피 정렬되어 있으니 중복된 값이 나와도 뒤만 보면 된다
-					visit[i] = true;
-					arr[depth] = sortArr[i];
-					before = sortArr[i];
-					dfs(depth + 1);
-					visit[i] = false;
-				}
-			}
-		}
-	}
+    private static void combi(int depth) {
+        if (depth == M) {
+            for (int num : list) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = 0; i < N; i++) {
+            // 방문하지 않았고, 이전값과 다르다면
+            if (!visited[i] && arr[i] != prev) {
+                visited[i] = true;
+                list.add(arr[i]);
+                prev = arr[i];
+
+                combi(depth + 1);
+
+                visited[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
 }
