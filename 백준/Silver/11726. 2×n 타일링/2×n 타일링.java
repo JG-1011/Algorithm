34261b05745eb043a38686753b11c0d3f6f407ae
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-19 21:56:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -5,14 +5,18 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1001]; // new int[n+1] 로 하면  런타임 에러(ArrayIndexOutOfBounds) 발생 new int[1001]로 바꿔줌 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+
+        for (int i = 3; i <= N; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
-        System.out.println(dp[n]);
+
+        System.out.println(dp[N]);
     }
 }
