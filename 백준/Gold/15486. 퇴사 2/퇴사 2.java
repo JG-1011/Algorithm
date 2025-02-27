d2f469fdcde48be71ecc10ae8d19473b357d681f
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-28 04:00:56 +0900

    [Gold V] Title: 퇴사 2, Time: 636 ms, Memory: 307880 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -3,33 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 뒤에서부터 해결?
- * 현재 상담 포함 가능한지 말지 판단 가능할듯?
- * */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
         int[] T = new int[N + 1];
         int[] P = new int[N + 1];
-        int[] dp = new int[N + 2];
 
         for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             T[i] = Integer.parseInt(st.nextToken());
             P[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = N; i > 0; i--) {
-            if (i + T[i] - 1 <= N) { // 완료o
-                dp[i] = Math.max(dp[i + 1], P[i] + dp[i + T[i]]);
-            } else { // 완료x
-                dp[i] = dp[i + 1];
+        int[] dp = new int[N + 2];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+
+            if (i + T[i] <= N + 1) { //예를 들어 N=7이고 7일 상담시간이 1일 경우도 가능해서 N+1일까지 봐야지
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
             }
         }
 
-        System.out.println(dp[1]);
+        System.out.println(Math.max(dp[N], dp[N + 1]));
     }
 }
