4c8a711e33bc2e3903132b4569434c00cc6a804c
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-02-27 17:31:57 +0900

    [Gold V] Title: 1로 만들기 2, Time: 96 ms, Memory: 19468 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        int[] path = new int[N + 1];
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            path[i] = i - 1;
+
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+                path[i] = i / 2;
+            }
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+                path[i] = i / 3;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(dp[N]).append("\n");
+        int cur = N;
+        while (cur != 0) {
+            sb.append(cur).append(" ");
+            cur = path[cur];
+        }
+
+        System.out.println(sb);
+    }
+}
