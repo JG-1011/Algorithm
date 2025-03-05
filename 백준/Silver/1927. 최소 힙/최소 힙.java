f8d546cbca2df5fc5d7d9453e21fe9bb78dfea9d
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-03-06 00:21:37 +0900

    [Silver II] Title: 최소 힙, Time: 244 ms, Memory: 25672 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,28 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
+            int x = Integer.parseInt(br.readLine());
 
-            if (num != 0) {
-                minHeap.add(num);
+            if (x == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
             } else {
-                if (minHeap.isEmpty()) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(minHeap.poll()).append("\n");
-                }
+                pq.add(x);
             }
         }
 
