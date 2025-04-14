2e854527a427045028dbbba7f2f7829881926cfe
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-04-14 17:05:05 +0900

    [level 3] Title: 여행경로, Time: 1.09 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,22 +2,24 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] tickets) {
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        HashMap<String, PriorityQueue<String>> graph = new HashMap<>();
+        
         for (String[] ticket : tickets) {
             graph.computeIfAbsent(ticket[0], k -> new PriorityQueue<>()).add(ticket[1]);
         }
-
+        
         List<String> path = new ArrayList<>();
         dfs("ICN", graph, path);
-
+        
         return path.toArray(new String[0]);
     }
-
-    private void dfs(String airport, Map<String, PriorityQueue<String>> graph, List<String> path) {
+    
+    public static void dfs(String airport, HashMap<String, PriorityQueue<String>> graph,  List<String> path) {
         while (graph.containsKey(airport) && !graph.get(airport).isEmpty()) {
             String next = graph.get(airport).poll();
             dfs(next, graph, path);
         }
+        
         path.add(0, airport);
     }
 }
