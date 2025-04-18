73db90abc6eaa2a69cf977f1a2319db8ca500b78
JG-1011 <116864863+JG-1011@users.noreply.github.com>
2025-04-18 17:17:42 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 18.89 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -4,9 +4,6 @@ class Solution {
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
     
-    static List<List<Point>> spaceList = new ArrayList<>();
-    static List<List<Point>> puzzleList = new ArrayList<>();
-    
     static class Point {
         int r, c;
         
@@ -16,98 +13,102 @@ class Solution {
         }
     }
     
+    static List<List<Point>> spaceList = new ArrayList<>();
+    static List<List<Point>> puzzleList = new ArrayList<>();
+    
     public int solution(int[][] game_board, int[][] table) {
-        int n = game_board.length;
-        boolean[][] visited = new boolean[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (game_board[i][j] == 0 && !visited[i][j]) {
-                    List<Point> list = new ArrayList<>();
-                    dfs(list, game_board, visited, n, i, j, 0);
-                    spaceList.add(normalize(list));
+        int N = game_board.length;
+        boolean[][] visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && game_board[i][j] == 0) {
+                    List<Point> tempList = new ArrayList<>();
+                    dfs(tempList, game_board, visited, i, j, N, 0);
+                    spaceList.add(normalize(tempList));
                 }
             }
         }
         
-        visited = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (table[i][j] == 1 && !visited[i][j]) {
-                    List<Point> list = new ArrayList<>();
-                    dfs(list, table, visited, n, i, j, 1);
-                    puzzleList.add(normalize(list));
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && table[i][j] == 1) {
+                    List<Point> tempList = new ArrayList<>();
+                    dfs(tempList, table, visited, i, j, N, 1);
+                    puzzleList.add(normalize(tempList));
                 }
             }
         }
-        
-        boolean[] used = new boolean[puzzleList.size()];
+
         int answer = 0;
-        
+        boolean[] used = new boolean[puzzleList.size()];
         for (List<Point> space : spaceList) {
             for (int i = 0; i < puzzleList.size(); i++) {
-                if(used[i]) continue;
+                if (used[i]) continue;
+                
                 List<Point> puzzle = puzzleList.get(i);
-                if(space.size() != puzzle.size()) continue;
+                
+                if (space.size() != puzzle.size()) continue;
                 
                 boolean match = false;
-                for (int r = 0; r < 4; r++) {
-                    if(isSame(space, puzzle)) {
+                for (int d = 0; d < 4; d++) {
+                    if (isSame(space, puzzle)) {
+                        used[i] = true;
+                        answer += space.size();
                         match = true;
                         break;
                     }
+                    
                     puzzle = rotate(puzzle);
                 }
                 
-                if(match) {
-                    used[i] = true;
-                    answer += space.size();
-                    break;
-                }
+                if (match) break;
             }
         }
         
         return answer;
     }
     
-    public static void dfs(List<Point> list, int[][] map, boolean[][] visited, int n, int startR, int startC, int target) {
-        visited[startR][startC] = true;
-        list.add(new Point(startR, startC));
+    public static void dfs(List<Point> tempList, int[][] map, boolean[][] visited, int r, int c, int N, int target) {
+        visited[r][c] = true;
+        tempList.add(new Point(r, c));
         
         for (int d = 0; d < 4; d++) {
-            int nr = startR + dr[d];
-            int nc = startC + dc[d];
+            int nr = r + dr[d];
+            int nc = c + dc[d];
             
-            if (nr < 0 || nc < 0 || nr >= n || nc >= n) continue;
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
             if (visited[nr][nc] || map[nr][nc] != target) continue;
             
-            dfs(list, map, visited, n, nr, nc, target);
+            dfs(tempList, map, visited, nr, nc, N, target);
         }
     }
     
-    public static List<Point> normalize(List<Point> list) {
-        List<Point> tempList = new ArrayList<>();
+    public static List<Point> normalize(List<Point> tempList) {
+        List<Point> result = new ArrayList<>();
+        
         int minR = Integer.MAX_VALUE;
         int minC = Integer.MAX_VALUE;
-        
-        for (Point p : list) {
+        for (Point p : tempList) {
             minR = Math.min(minR, p.r);
             minC = Math.min(minC, p.c);
         }
         
-        for (Point p : list) {
-            tempList.add(new Point(p.r - minR, p.c - minC));
+        for (Point p : tempList) {
+            result.add(new Point(p.r - minR, p.c - minC));
         }
         
-        tempList.sort((a, b) -> {
+        result.sort((a, b) -> {
             if (a.r == b.r) return a.c - b.c;
             return a.r - b.r;
         });
         
-        return tempList;
+        return result;
     }
     
     public static boolean isSame(List<Point> space, List<Point> puzzle) {
+        
         for (int i = 0; i < space.size(); i++) {
             if(space.get(i).r != puzzle.get(i).r || space.get(i).c != puzzle.get(i).c) return false;
         }
@@ -116,17 +117,17 @@ class Solution {
     }
     
     public static List<Point> rotate(List<Point> puzzle) {
-        List<Point> tempList = new ArrayList<>();
+        List<Point> result = new ArrayList<>();
         
         for (Point p : puzzle) {
-            tempList.add(new Point(p.c, -p.r));
+            result.add(new Point(p.c, -p.r));
         }
         
-        tempList.sort((a, b) -> {
+        result.sort((a, b) -> {
             if (a.r == b.r) return a.c - b.c;
             return a.r - b.r;
         });
         
-        return normalize(tempList);
+        return normalize(result);
     }
 }
